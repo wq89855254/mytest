2e9ad8bf2d1866a5782ec5556cb074f4d19d989c
wenq <bindwe@163.com>
2021-03-04 18:20:33 +0800

    '添加d3'

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/Home.vue'
+import D3 from './views/d3/D3.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect:'d3',
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/d3',
+      name: 'd3',
+      component: D3
     },
   
   ]
---
+++ src/views/d3/D3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="D3">
+    <div class="test">123</div>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+export default {
+  name: "D3",
+  data(){
+    return{}
+  },
+  created() {
+    console.log(d3.select('.test').html())
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
